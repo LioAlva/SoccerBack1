a29ee4f54d5050398f403ae89967c5199d0fa9c3
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-09-30 14:32:47 -0500

    Soccer Backend creado video 2 instalando ademas Identity:
    Install-Package Microsoft.AspNet.Identity.Samples -Version 2.1.0-alpha1

---
+++ SoccerBack1/Backend/Controllers/LeaguesController.cs
@@ -0,0 +1,123 @@
+﻿using Domain;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Backend.Controllers
+{
+    public class LeaguesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Leagues
+        public ActionResult Index()
+        {
+            return View(db.Leagues.ToList());
+        }
+
+        // GET: Leagues/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // GET: Leagues/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Leagues/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LeagueId,Name,Logo")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Leagues.Add(league);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(league);
+        }
+
+        // GET: Leagues/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // POST: Leagues/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LeagueId,Name,Logo")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(league).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(league);
+        }
+
+        // GET: Leagues/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // POST: Leagues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            League league = db.Leagues.Find(id);
+            db.Leagues.Remove(league);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
